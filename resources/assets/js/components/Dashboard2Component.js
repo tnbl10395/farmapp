97a95a03fbe970c920d531245e3ba4f55e0a84d6
tnbl10395 <tnbaolongit@gmail.com>
2018-04-16 13:07:47 +0700

    save

--- resources/assets/js/components/Dashboard2Component.js
+++ resources/assets/js/components/Dashboard2Component.js
@@ -647,7 +647,7 @@ class Info extends React.Component {
                     <div style={styleInfo.content}>
                         <p>Rice, a monocot, is normally grown as an annual plant, although in tropical areas it can survive as a perennial and can produce a ratoon crop for up to 30 years.[3] Rice cultivation is well-suited to countries and regions with low labor costs and high rainfall, as it is labor-intensive to cultivate and requires ample water. However, rice can be grown practically anywhere, even on a steep hill or mountain area with the use of water-controlling terrace systems. Although its parent species are native to Asia and certain parts of Africa, centuries of trade and exportation have made it commonplace in many cultures worldwide.</p>
                     </div>
-                </div>0
+                </div>
             </div>
         )
     }
